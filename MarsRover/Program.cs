a67254140ae83aaf88c943fd47634731257f3ac5
Onur ŞAFAK <onursafak93@gmail.com>
2019-12-26 11:18:25 +0300

    Update main func.

--- MarsRover/Program.cs
+++ MarsRover/Program.cs
@@ -12,11 +12,11 @@ namespace MarsRover
 
             Plateau plateauOne = new Plateau(new Position(5, 5));
             Rover firstRover = new Rover(plateauOne, new Position(1, 2), Directions.N);
-            firstRover.Process("LMLMLMLMMMM");
+            firstRover.Process("LMLMLMLMM");
 
-            //Second rover 
-            Plateau plateauTwo = new Plateau(new Position(5, 5));
-            Rover secondRover = new Rover(plateauTwo, new Position(3, 3), Directions.E);
+             
+            
+            Rover secondRover = new Rover(plateauOne, new Position(3, 3), Directions.E);
             secondRover.Process("MMRMMRMRRM");
 
             Console.WriteLine(firstRover.ToString());
